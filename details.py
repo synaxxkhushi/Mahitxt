5dcac665df8577cbae5e71156a676e152dd7ae50
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2024-08-25 23:37:39 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 25263708
-api_hash = "9bde48b267ce65a576b478c0ff8c7bbb"
+api_id = 24267726
+api_hash = "7500ba8248548cc3864bd033668f9a9a"
 bot_token = "7073327110:AAGFGVPzzqquAQNMBXACtyZRv5UuVvOIGgg"
 
